0c3bcda291abe804e6ef174d29c8ed171905a820
Sakuzuna <nikita.kopychev@s.wingateschool.com>
2025-02-25 23:58:34 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import argparse
 from pystyle import Colorate, Colors
+from banner import sharkprint
 from utils.proxy_utils import load_proxies
 from workers.http_flood_attack import http_flood_attack
 from workers.http_bypass_attack import http_bypass_attack
 from workers.tcp_flood_attack import tcp_flood_attack
-from banner import sharkprint
 
 COLOR_CODE = {
     "RESET": "\033[0m",  
@@ -31,17 +31,18 @@ def main():
     parser.add_argument("target", help="Target URL or IP:Port")
     parser.add_argument("--attack", choices=["http_flood", "http_bypass", "tcp_flood"], required=True, help="Type of attack")
     parser.add_argument("--duration", type=int, default=60, help="Duration of attack in seconds")
+    parser.add_argument("--packet-size", type=int, default=64, help="Packet size in KB (default: 64)")
     args = parser.parse_args()
 
     proxies = load_proxies("data/proxies.txt")
     user_agents = load_user_agents("data/uas.txt")
 
     if args.attack == "http_flood":
-        http_flood_attack(args.target, proxies, user_agents, args.duration)
+        http_flood_attack(args.target, proxies, user_agents, args.duration, args.packet_size)
     elif args.attack == "http_bypass":
-        http_bypass_attack(args.target, proxies, user_agents, args.duration)
+        http_bypass_attack(args.target, proxies, user_agents, args.duration, args.packet_size)
     elif args.attack == "tcp_flood":
-        tcp_flood_attack(args.target, proxies, args.duration)
+        tcp_flood_attack(args.target, proxies, args.duration, args.packet_size)
 
 if __name__ == "__main__":
     main()
