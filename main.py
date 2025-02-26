e829950091215378ba3455c53051005cf886f8b1
Sakuzuna <nikita.kopychev@s.wingateschool.com>
2025-02-26 22:19:03 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import argparse
+import asyncio
 from pystyle import Colorate, Colors
 from banner import sharkprint
 from utils.proxy_utils import load_proxies
 from workers.http_flood_attack import http_flood_attack
-from workers.http_bypass_attack import http_bypass_attack
-from workers.tcp_flood_attack import tcp_flood_attack
 
 COLOR_CODE = {
     "RESET": "\033[0m",  
@@ -25,12 +24,11 @@ def load_user_agents(file_path):
     with open(file_path, "r") as file:
         return file.read().splitlines()
 
-def main():
+async def main():
     print(Colorate.Horizontal(Colors.blue_to_white, (sharkprint)))
-    print("python main.py http://example.com --attack http_flood --duration 60 --packet-size 128 --packet-delay 200")
     parser = argparse.ArgumentParser(description="Network Stress Tool")
     parser.add_argument("target", help="Target URL or IP:Port")
-    parser.add_argument("--attack", choices=["http_flood", "http_bypass", "tcp_flood"], required=True, help="Type of attack")
+    parser.add_argument("--attack", choices=["http_flood"], required=True, help="Type of attack")
     parser.add_argument("--duration", type=int, default=60, help="Duration of attack in seconds")
     parser.add_argument("--packet-size", type=int, default=64, help="Packet size in KB (default: 64)")
     parser.add_argument("--packet-delay", type=int, default=100, help="Delay between packets in milliseconds (default: 100)")
@@ -40,11 +38,7 @@ def main():
     user_agents = load_user_agents("data/uas.txt")
 
     if args.attack == "http_flood":
-        http_flood_attack(args.target, proxies, user_agents, args.duration, args.packet_size, args.packet_delay)
-    elif args.attack == "http_bypass":
-        http_bypass_attack(args.target, proxies, user_agents, args.duration, args.packet_size, args.packet_delay)
-    elif args.attack == "tcp_flood":
-        tcp_flood_attack(args.target, proxies, args.duration, args.packet_size, args.packet_delay)
+        await http_flood_attack(args.target, proxies, user_agents, args.duration, args.packet_size, args.packet_delay)
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
