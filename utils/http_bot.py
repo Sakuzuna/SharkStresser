396078cf12f2df34b1741fc8a49f4d64898ff739
Sakuzuna <nikita.kopychev@s.wingateschool.com>
2025-02-26 00:05:25 +0000

    Update http_bot.py

--- utils/http_bot.py
+++ utils/http_bot.py
@@ -14,13 +14,16 @@ class HTTPBot:
     def get_random_user_agent(self):
         return random.choice(self.user_agents)
 
-    def send_request(self, url):
+    def send_request(self, url, payload=None):
         proxy = self.get_random_proxy()
         user_agent = self.get_random_user_agent()
         session = HttpClient.create_session(proxy, user_agent)
 
         try:
-            response = session.get(url, timeout=5)
-            print(f"🦈 Shark ate a fish: {url} using proxy: {proxy['host']}:{proxy['port']} successful. Nom-Nom")
+            if payload:
+                response = session.post(url, data=payload, timeout=5)
+            else:
+                response = session.get(url, timeout=5)
+            print(f"🦈 Shark successfully attacked the fish: {url} frоm: {proxy['host']}:{proxy['port']} Nom-Nom!")
         except Exception as e:
-            print(f"🐟 The fish: {url} escaped the shark using proxy: {proxy['host']}:{proxy['port']}. Go try cathc a dinner!: {e}")
+            print(f"🐟 Oh No! The fish: {url} escaped from: {proxy['host']}:{proxy['port']} Get better proxies!: {e}")
