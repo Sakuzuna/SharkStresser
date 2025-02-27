86f2962f97b55a87c73551f01b138a5f54f93b78
Sakuzuna <nikita.kopychev@s.wingateschool.com>
2025-02-27 00:24:37 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,16 @@
 import argparse
-from pystyle import Colorate, Colors
 from banner import sharkprint
 from utils.proxy_utils import load_proxies
 from workers.http_flood_attack import http_flood_attack
 from workers.http_bypass_attack import http_bypass_attack
 from workers.tcp_flood_attack import tcp_flood_attack
 
-COLOR_CODE = {
-    "RESET": "\033[0m",  
-    "UNDERLINE": "\033[04m",
-    "GREEN": "\033[32m",     
-    "YELLOW": "\033[93m",    
-    "RED": "\033[31m",       
-    "CYAN": "\033[36m",     
-    "BOLD": "\033[01m",        
-    "PINK": "\033[95m",
-    "URL_L": "\033[36m",       
-    "LI_G": "\033[92m",      
-    "F_CL": "\033[0m",
-    "DARK": "\033[90m",     
-}
-
 def load_user_agents(file_path):
     with open(file_path, "r") as file:
         return file.read().splitlines()
 
 def main():
-    print(Colorate.Horizontal(Colors.blue_to_white, (sharkprint)))
+    print(sharkprint)
     parser = argparse.ArgumentParser(description="Network Stress Tool")
     parser.add_argument("target", help="Target URL or IP:Port")
     parser.add_argument("--attack", choices=["http_flood", "http_bypass", "tcp_flood"], required=True, help="Type of attack")
